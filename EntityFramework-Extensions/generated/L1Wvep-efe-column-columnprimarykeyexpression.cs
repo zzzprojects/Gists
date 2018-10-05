beb9c91a0d9305c128eebc69535eb1fb8680d1cf
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:09 -0400

    ADD Fiddle: L1Wvep

---
+++ EntityFramework-Extensions/generated/L1Wvep-efe-column-columnprimarykeyexpression.cs
@@ -0,0 +1,96 @@
+// Description: EFE - Column - ColumnPrimaryKeyExpression
+// Website: https://entityframework-extensions.net/
+// Run: https://dotnetfiddle.net/L1Wvep
+
+// @nuget: EntityFramework
+// @nuget: EntityFramework.SqlServerCompact
+// @nuget: Microsoft.SqlServer.Compact
+// @nuget: Z.EntityFramework.Extensions
+
+using System.Linq;
+using System.Data.Entity;
+using System.Data.SqlServerCe;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Custom configuration specific for .NET Fiddle
+		Z.EntityFramework.Extensions.EntityFrameworkManager.UseFiddleSqlCompact(System.Data.Entity.SqlServerCompact.SqlCeProviderServices.Instance, System.Data.SqlServerCe.SqlCeProviderFactory.Instance);
+		
+		// An index is required for the key only for SQL Compact.
+		CreateBDWithIndex();
+		GenerateData();
+		
+		var list = new List<Customer>() {	new Customer () { Name = "Customer_B", Description = "Updated_B", Login = "Login2", Password = "Password2" },
+			new Customer() { Name ="Customer_C", Description = "Description", Login = "Login3", Password = "Password3" }};
+	
+			
+		// Column Primary Key: Specify custom keys 
+		using (var context = new EntityContext())
+		{
+			context.BulkMerge(list, options => options.ColumnPrimaryKeyExpression = customer => new { customer.Login, customer.Password });
+			
+			
+			FiddleHelper.WriteTable(context.Customers.ToList());
+		}
+	}
+		 
+	public static void GenerateData()
+	{
+		
+		using (var context = new EntityContext())
+		{
+			context.Customers.Add(new Customer() { Name ="Customer_A", Description = "Description", Login = "Login1", Password = "Password1" });
+			context.Customers.Add(new Customer() { Name ="Customer_B", Description = "Description" , Login = "Login2", Password = "Password2" });
+			context.BulkSaveChanges();	
+		}
+		
+		using (var context = new EntityContext())
+		{
+			FiddleHelper.WriteTable(context.Customers.ToList());	
+		}
+	}
+	
+		
+	public static void CreateBDWithIndex()
+	{
+		using (var context = new EntityContext())
+		{
+			context.Database.CreateIfNotExists();
+			
+		}
+		using (var connection = new SqlCeConnection(@"Data Source=ZZZ_Projects.sdf"))
+		{
+			connection.Open();
+			using (var command = connection.CreateCommand())
+			{
+				command.CommandText = "CREATE UNIQUE INDEX UX_Login ON Customers(Login, Password)";
+				command.ExecuteNonQuery();
+			}
+		}	
+	}
+	
+	
+
+	public class EntityContext : DbContext
+	{
+		public EntityContext() : base(@"Data Source=ZZZ_Projects.sdf")
+		{
+
+		}
+		
+		public DbSet<Customer> Customers { get; set; }
+	}
+	
+	
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Login  { get; set; }
+		public string Password { get; set; }
+	}
+}
