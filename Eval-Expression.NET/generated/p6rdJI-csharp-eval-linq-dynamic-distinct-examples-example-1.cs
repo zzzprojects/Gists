fa79aab7d01833823978240ee478891d989d2118
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:18 -0400

    ADD Fiddle: p6rdJI

---
+++ Eval-Expression.NET/generated/p6rdJI-csharp-eval-linq-dynamic-distinct-examples-example-1.cs
@@ -0,0 +1,22 @@
+// Description: C# Eval - linq-dynamic-distinct-examples - Example 1
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/p6rdJI
+
+using System;
+using System.Linq;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] factorsOf300 = {2, 2, 3, 5, 5};
+
+		var uniqueFactors = factorsOf300.Distinct();
+
+		Console.WriteLine("Prime factors of 300:");
+		foreach (var f in uniqueFactors)
+		{
+			Console.WriteLine(f.ToString());
+		}
+	}
+}
