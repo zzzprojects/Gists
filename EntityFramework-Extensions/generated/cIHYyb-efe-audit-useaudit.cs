1dc1a014846fced9c65e5e8b6c7ddf8d9ee8ce2d
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:37 -0400

    ADD Fiddle: cIHYyb

---
+++ EntityFramework-Extensions/generated/cIHYyb-efe-audit-useaudit.cs
@@ -0,0 +1,95 @@
+// Description: EFE - Audit - UseAudit
+// Website: https://entityframework-extensions.net/
+// Run: https://dotnetfiddle.net/cIHYyb
+
+// @nuget: EntityFramework
+// @nuget: EntityFramework.SqlServerCompact
+// @nuget: Microsoft.SqlServer.Compact
+// @nuget: Z.EntityFramework.Extensions
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using Z.BulkOperations;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Custom configuration specific for .NET Fiddle
+		Z.EntityFramework.Extensions.EntityFrameworkManager.UseFiddleSqlCompact(System.Data.Entity.SqlServerCompact.SqlCeProviderServices.Instance, System.Data.SqlServerCe.SqlCeProviderFactory.Instance);
+
+		GenerateData();		
+
+		// UseAudit : Gets or sets if INSERTED and DELETED data from the database should be returned as AuditEntries.
+		using (var context = new EntityContext())
+		{
+			var listToRemove = context.Customers.Where(x => x.IsActive == false ).ToList();
+			var listToModify = context.Customers.Where(x => x.IsActive == true ).ToList();
+			var listToAdd = new List<Customer>() { new Customer() { Name = "Customer_C", Description = "Description", IsActive = false}};
+			
+			
+			context.Customers.AddRange(listToAdd); // add
+			context.Customers.RemoveRange(listToRemove); // remove
+			listToModify.First().Description = "Updated_A"; // modify
+			
+			List<AuditEntry> auditEntries = new List<AuditEntry>();
+			
+			context.BulkSaveChanges(options =>
+			{
+				options.UseAudit = true;
+				options.BulkOperationExecuted = bulkOperation => auditEntries.AddRange(bulkOperation.AuditEntries);
+			});
+			
+			FiddleHelper.WriteTable(auditEntries);
+			
+			foreach(var audit in auditEntries)
+			{
+				FiddleHelper.WriteTable(audit.Values);
+			}
+		}
+	}
+		 
+	public static void GenerateData()
+	{
+		
+		using (var context = new EntityContext())
+		{
+			context.Customers.Add(new Customer() { Name ="Customer_A", Description = "Description", IsActive = true });
+			context.Customers.Add(new Customer() { Name ="Customer_B", Description = "Description", IsActive = false });
+			context.BulkSaveChanges();	
+		}
+	}
+	
+	public static List<Customer> GenerateListCustomer() 
+	{
+		var customers = new List<Customer>();
+		
+		for (int i = 0; i < 1000; i++)
+		{
+			customers.Add(new Customer() { Name ="Customer_" + i, Description = "Description", IsActive = true });
+		}
+		
+		return customers;
+	}
+
+	public class EntityContext : DbContext
+	{
+		public EntityContext() : base(@"Data Source=ZZZ_Projects.sdf")
+		{
+
+		}
+		
+		public DbSet<Customer> Customers { get; set; }
+	}
+	
+	
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public Boolean IsActive { get; set; }
+	}
+}
