c16e8cdcc751b6c9c7bc2b938e46c9d533dcc404
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:52 -0400

    ADD Fiddle: HbOEOt

---
+++ Eval-Expression.NET/generated/HbOEOt-csharp-eval-aggregate-example-2.cs
@@ -0,0 +1,18 @@
+// Description: C# Eval - aggregate - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/HbOEOt
+
+using System;
+using Z.Expressions;
+
+public class Program
+{
+	public static void Main()
+	{
+		double[] doubles = {1.7, 2.3, 1.9, 4.1, 2.9};
+
+		var product = doubles.Execute<double>("Aggregate((runningProduct, nextFactor) => runningProduct * nextFactor)");
+		
+		Console.WriteLine("Total product of all numbers: {0}", product);
+	}
+}
