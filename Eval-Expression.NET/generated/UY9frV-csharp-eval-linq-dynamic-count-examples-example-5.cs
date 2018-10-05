4bc72d67592b43828b2ab92f0431f75d02fbb31a
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:37 -0400

    ADD Fiddle: UY9frV

---
+++ Eval-Expression.NET/generated/UY9frV-csharp-eval-linq-dynamic-count-examples-example-5.cs
@@ -0,0 +1,23 @@
+// Description: C# Eval - linq-dynamic-count-examples - Example 5
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/UY9frV
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+		int[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+	
+		var oddNumbers = numbers.Execute<int>("Count(n => n % 2 == 1)");
+	
+		Console.WriteLine("There are {0} odd numbers in the list.", oddNumbers);
+		
+	}
+}
