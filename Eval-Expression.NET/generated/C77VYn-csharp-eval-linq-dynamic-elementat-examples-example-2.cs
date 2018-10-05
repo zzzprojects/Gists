61cb96a803ecb739440c5ffd5df15fd40e0b77af
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:34 -0400

    ADD Fiddle: C77VYn

---
+++ Eval-Expression.NET/generated/C77VYn-csharp-eval-linq-dynamic-elementat-examples-example-2.cs
@@ -0,0 +1,21 @@
+// Description: C# Eval - linq-dynamic-elementat-examples - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/C77VYn
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using System.Linq;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+
+		var fourthLowNum = numbers.Where(n => n > 5).Execute<int>("ElementAt(1)"); // second number is index 1 because sequences use 0-based indexing 
+		
+		Console.WriteLine("Second number > 5: {0}", fourthLowNum);
+	}
+}
