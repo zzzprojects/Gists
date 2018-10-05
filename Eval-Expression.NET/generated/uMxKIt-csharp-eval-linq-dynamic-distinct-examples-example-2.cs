3f239329fbc7e940537a2d765efb19b7df86d195
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:36 -0400

    ADD Fiddle: uMxKIt

---
+++ Eval-Expression.NET/generated/uMxKIt-csharp-eval-linq-dynamic-distinct-examples-example-2.cs
@@ -0,0 +1,26 @@
+// Description: C# Eval - linq-dynamic-distinct-examples - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/uMxKIt
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using System.Collections.Generic;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] factorsOf300 = {2, 2, 3, 5, 5};
+	
+		var uniqueFactors = factorsOf300.Execute<IEnumerable<int>>("Distinct()");
+	
+	
+		Console.WriteLine("Prime factors of 300:");
+		foreach (var f in uniqueFactors)
+		{
+			Console.WriteLine(f.ToString());
+		}
+	}
+}
