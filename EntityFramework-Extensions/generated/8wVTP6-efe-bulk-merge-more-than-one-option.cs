be9cb9ac3a198ce032f78b551de420aa97a03a94
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:26 -0400

    ADD Fiddle: 8wVTP6

---
+++ EntityFramework-Extensions/generated/8wVTP6-efe-bulk-merge-more-than-one-option.cs
@@ -0,0 +1,82 @@
+// Description: EFE - Bulk-Merge - More than one option
+// Website: https://entityframework-extensions.net/
+// Run: https://dotnetfiddle.net/8wVTP6
+
+// @nuget: EntityFramework
+// @nuget: EntityFramework.SqlServerCompact
+// @nuget: Microsoft.SqlServer.Compact
+// @nuget: Z.EntityFramework.Extensions
+
+using System;
+using System.Linq;
+using System.Data.Entity;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Custom configuration specific for .NET Fiddle
+		Z.EntityFramework.Extensions.EntityFrameworkManager.UseFiddleSqlCompact(System.Data.Entity.SqlServerCompact.SqlCeProviderServices.Instance, System.Data.SqlServerCe.SqlCeProviderFactory.Instance);
+
+		GenerateData();
+		
+
+		// BulkMerge: Specify more than one option
+		using (var context = new EntityContext())
+		{
+			var list = context.Customers.ToList();
+			
+			list.First().Description = "Updated_A";
+			list.First().IsActive = false;
+			list.First().Name = "NotUpdated";
+			
+			list.Add(new Customer() { Name ="Customer_B", Description = "Description"});
+			
+			
+			context.BulkMerge(list, options => {
+					options.BatchSize = 100;
+					options.ColumnInputExpression = c => new {c.CustomerID, c.Description, c.IsActive};
+				});
+		}
+		
+		using (var context = new EntityContext())
+		{
+			FiddleHelper.WriteTable(context.Customers.ToList());	
+		}
+	}
+		 
+	public static void GenerateData()
+	{
+		
+		using (var context = new EntityContext())
+		{
+			context.Customers.Add(new Customer() { Name ="Customer_A", Description = "Description", IsActive = true });
+
+			context.BulkSaveChanges();	
+		}
+		
+		using (var context = new EntityContext())
+		{
+			FiddleHelper.WriteTable(context.Customers.ToList());	
+		}
+	}
+
+	public class EntityContext : DbContext
+	{
+		public EntityContext() : base(@"Data Source=ZZZ_Projects.sdf")
+		{
+
+		}
+		
+		public DbSet<Customer> Customers { get; set; }
+	}
+	
+	
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public Boolean IsActive { get; set; }
+	}
+}
