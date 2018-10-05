bbeabfdb7748e08a19a3373f5e9952565cd0cb81
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:21 -0400

    ADD Fiddle: pqhtyM

---
+++ Eval-Expression.NET/generated/pqhtyM-csharp-eval-linq-dynamic-first-examples-example-5.cs
@@ -0,0 +1,20 @@
+// Description: C# Eval - linq-dynamic-first-examples - Example 5
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/pqhtyM
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] strings = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+		var startsWithO =  strings.Execute<string>("First(s => s[0] == 'o')");
+
+		Console.WriteLine("A string starting with 'o': {0}", startsWithO);
+	}
+}
