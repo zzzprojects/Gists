69bf9e88f08a296ba9f90f976d3a48ddf2aa7f23
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:25 -0400

    ADD Fiddle: 8mtLH8

---
+++ Eval-Expression.NET/generated/8mtLH8-csharp-eval-eval-execute-example-2.cs
@@ -0,0 +1,40 @@
+// Description: C# Eval - eval-execute - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/8mtLH8
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Z.Expressions;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Parameter: Anonymous Type
+		object result = Eval.Execute("X + Y", new { X = 1, Y = 2} );
+		
+		Console.WriteLine(result);
+		
+		// Parameter: Argument Position
+		result = Eval.Execute("{0} + {1}", 1, 2);
+		
+		Console.WriteLine(result);
+		
+		// Parameter: Class Member
+		dynamic expandoObject = new ExpandoObject();
+		expandoObject.X = 1;
+		expandoObject.Y = 2;
+		result = Eval.Execute("X + Y", expandoObject);
+
+		Console.WriteLine(result);
+		
+		// Parameter: Dictionary Key
+		var values = new Dictionary<string, object>() { {"X", 1}, {"Y", 2} };
+		result = Eval.Execute("X + Y", values);
+								
+		Console.WriteLine(result);
+	}
+}
