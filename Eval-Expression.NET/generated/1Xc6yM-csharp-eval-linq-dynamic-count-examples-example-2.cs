5ad91b2a4f7b581789b5575eca3715e490db3f3f
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:12 -0400

    ADD Fiddle: 1Xc6yM

---
+++ Eval-Expression.NET/generated/1Xc6yM-csharp-eval-linq-dynamic-count-examples-example-2.cs
@@ -0,0 +1,21 @@
+// Description: C# Eval - linq-dynamic-count-examples - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/1Xc6yM
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using System.Linq;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] factorsOf300 = {2, 2, 3, 5, 5};
+	
+		var uniqueFactors = factorsOf300.Distinct().Execute<int>("Count()");
+	
+		Console.WriteLine("There are {0} unique factors of 300.", uniqueFactors);
+	}
+}
