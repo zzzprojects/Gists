399f62072da46aa38ac1c741d538b99596522376
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:14 -0400

    ADD Fiddle: ML8HVw

---
+++ Eval-Expression.NET/generated/ML8HVw-csharp-eval-linq-dynamic-elementat-examples-example-1.cs
@@ -0,0 +1,18 @@
+// Description: C# Eval - linq-dynamic-elementat-examples - Example 1
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/ML8HVw
+
+using System;
+using System.Linq;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] numbers = {5, 4, 1, 3, 9, 8, 6, 7, 2, 0};
+
+		var fourthLowNum = numbers.Where(n => n > 5).ElementAt(1); // second number is index 1 because sequences use 0-based indexing 
+
+		Console.WriteLine("Second number > 5: {0}", fourthLowNum);
+	}
+}
