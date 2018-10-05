0ffc68b4ba487a3b66f1beff67673a97ea12540e
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:34 -0400

    ADD Fiddle: BZLbt5

---
+++ Eval-Expression.NET/generated/BZLbt5-csharp-eval-linq-dynamic-aggregate-examples-example-1.cs
@@ -0,0 +1,18 @@
+// Description: C# Eval - linq-dynamic-aggregate-examples - Example 1
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/BZLbt5
+
+using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		double[] doubles = {1.7, 2.3, 1.9, 4.1, 2.9};
+
+		var product = doubles.Aggregate((runningProduct, nextFactor) => runningProduct * nextFactor);
+	
+		Console.WriteLine("Total product of all numbers: {0}", product);
+	}
+}
