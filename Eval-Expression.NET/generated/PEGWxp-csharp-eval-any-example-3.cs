3e95c82d96f097e8f83c8639c71e2b17b46d8339
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:19 -0400

    ADD Fiddle: PEGWxp

---
+++ Eval-Expression.NET/generated/PEGWxp-csharp-eval-any-example-3.cs
@@ -0,0 +1,20 @@
+// Description: C# Eval - any - Example 3
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/PEGWxp
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] words = {"believe", "relief", "receipt", "field"};
+	
+		var iAfterE = words.Execute<bool>("Any(w => w.Contains('ei'))");		
+		
+		Console.WriteLine("There is a word that contains in the list that contains 'ei': {0}", iAfterE);
+	}
+}
