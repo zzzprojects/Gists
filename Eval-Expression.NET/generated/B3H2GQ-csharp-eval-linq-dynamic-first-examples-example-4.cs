82a9e4c359affc501597944670861e8eed6bdfc6
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:29 -0400

    ADD Fiddle: B3H2GQ

---
+++ Eval-Expression.NET/generated/B3H2GQ-csharp-eval-linq-dynamic-first-examples-example-4.cs
@@ -0,0 +1,21 @@
+// Description: C# Eval - linq-dynamic-first-examples - Example 4
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/B3H2GQ
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using System.Linq;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] strings = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+		var startsWithO = strings.First(s => "s[0] == 'o'");
+
+		Console.WriteLine("A string starting with 'o': {0}", startsWithO);
+	}
+}
