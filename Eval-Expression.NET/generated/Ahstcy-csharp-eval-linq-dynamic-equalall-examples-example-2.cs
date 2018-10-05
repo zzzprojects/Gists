315d98317994c93dc1e9f803d2bf4f23804e8703
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:28 -0400

    ADD Fiddle: Ahstcy

---
+++ Eval-Expression.NET/generated/Ahstcy-csharp-eval-linq-dynamic-equalall-examples-example-2.cs
@@ -0,0 +1,21 @@
+// Description: C# Eval - linq-dynamic-equalall-examples - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/Ahstcy
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var wordsA = new[] {"cherry", "apple", "blueberry"};
+		var wordsB = new[] {"cherry", "apple", "blueberry"};
+		
+		var match = wordsA.Execute<bool>("SequenceEqual(wordsB)", new {wordsB});
+		
+		Console.WriteLine("The sequences match: {0}", match);
+	}
+}
