4ef3ed8b4d18f0eebe475f4adf71240927bbe6d2
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:24 -0400

    ADD Fiddle: 8kTOjI

---
+++ Eval-Expression.NET/generated/8kTOjI-csharp-eval-linq-dynamic-all-examples-example-3.cs
@@ -0,0 +1,21 @@
+// Description: C# Eval - linq-dynamic-all-examples - Example 3
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/8kTOjI
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+
+public class Program
+{
+	public static void Main()
+	{
+		int[] numbers = {1, 11, 3, 19, 41, 65, 19};
+
+		var onlyOdd = numbers.Execute<bool>("All(n => n % 2 == 1)");
+		
+		Console.WriteLine("The list contains only odd numbers: {0}", onlyOdd);
+	}
+}
