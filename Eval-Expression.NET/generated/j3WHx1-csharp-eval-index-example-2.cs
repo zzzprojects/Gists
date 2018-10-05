6b3658c49b46414dfe417e8e14d1b4362088069d
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:57 -0400

    ADD Fiddle: j3WHx1

---
+++ Eval-Expression.NET/generated/j3WHx1-csharp-eval-index-example-2.cs
@@ -0,0 +1,22 @@
+// Description: C# Eval - index - Example 2
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/j3WHx1
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int result = Eval.Execute<int>(@"
+		var list = new List<int>() { 1, 2, 3, 4, 5 };
+		return list.Where(x => x > X).Take(Y).Count();", new { X = 1, Y = 2});
+				
+		Console.WriteLine(result);
+				
+		
+	}
+}
