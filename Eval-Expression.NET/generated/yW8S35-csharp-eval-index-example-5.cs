c6a2f24c8cfb316ac27016818307317154e29a02
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:52:48 -0400

    ADD Fiddle: yW8S35

---
+++ Eval-Expression.NET/generated/yW8S35-csharp-eval-index-example-5.cs
@@ -0,0 +1,28 @@
+// Description: C# Eval - index - Example 5
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/yW8S35
+
+// @nuget: Z.Expressions.Eval
+
+using System;
+using Z.Expressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var customer = new Customer() { Name = "ZZZ" };
+		
+		var nameGetter = Eval.Compile<Func<Customer, string>>("x.Name", "x");
+		var name = nameGetter(customer);
+				
+		Console.WriteLine(name);
+				
+		
+	}
+	
+	public class Customer
+	{
+		public String Name {get;set;}
+	}
+}
