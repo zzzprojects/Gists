716755f3a3d2feef77cb71eed66b74b6ce13c2b5
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:42 -0400

    ADD Fiddle: E82Y4a

---
+++ Eval-Expression.NET/generated/E82Y4a-csharp-eval-linq-dynamic-all-examples-example-1.cs
@@ -0,0 +1,18 @@
+// Description: C# Eval - linq-dynamic-all-examples - Example 1
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/E82Y4a
+
+using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		int[] numbers = {1, 11, 3, 19, 41, 65, 19};
+		
+		var onlyOdd = numbers.All(n => n % 2 == 1);
+		
+		Console.WriteLine("The list contains only odd numbers: {0}", onlyOdd);
+	}
+}
