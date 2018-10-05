456a8f10f8460dd1dfd455cae8c5ca8df760be40
Jonathan Magnan <info@zzzprojects.com>
2018-10-05 14:51:15 -0400

    ADD Fiddle: 36Tkzn

---
+++ Eval-Expression.NET/generated/36Tkzn-csharp-eval-any-example-1.cs
@@ -0,0 +1,19 @@
+// Description: C# Eval - any - Example 1
+// Website: https://eval-expression.net/
+// Run: https://dotnetfiddle.net/36Tkzn
+
+using System;
+using System.Linq;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string[] words = {"believe", "relief", "receipt", "field"};
+	
+		var iAfterE = words.Any(w => w.Contains("ei"));
+	
+		Console.WriteLine("There is a word that contains in the list that contains 'ei': {0}", iAfterE);
+
+	}
+}
