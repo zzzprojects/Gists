7bac3758ec15cbc165ce0a30aa948ec1d2af6b12
Jonathan Magnan <info@zzzprojects.com>
2018-10-06 12:52:07 -0400

    ADD Fiddle: EsvZyg

---
+++ html-agility-pack/generated/EsvZyg-hap-load--from-file-.cs
@@ -0,0 +1,48 @@
+// Description: HAP - Load (From File)
+// Website: https://html-agility-pack.net/
+// Run: https://dotnetfiddle.net/EsvZyg
+
+// @nuget: HtmlAgilityPack
+
+using System;
+using System.Xml;
+using HtmlAgilityPack;
+					
+public class Program
+{
+	public static void Main()
+	{
+		SaveHtmlFile();
+		
+		#region example
+			
+		var path = @"test.html";
+		
+		var doc = new HtmlDocument();
+		doc.Load(path);
+		
+		var node = doc.DocumentNode.SelectSingleNode("//body");
+
+        Console.WriteLine(node.OuterHtml);	
+		
+		#endregion
+	}
+	
+	private static void SaveHtmlFile()
+	{
+		var html =
+        @"<!DOCTYPE html>
+<html>
+<body>
+	<h1>This is <b>bold</b> heading</h1>
+	<p>This is <u>underlined</u> paragraph</p>
+	<h2>This is <i>italic</i> heading</h2>
+</body>
+</html> ";
+
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(html);
+		
+		htmlDoc.Save("test.html");
+	}
+}
